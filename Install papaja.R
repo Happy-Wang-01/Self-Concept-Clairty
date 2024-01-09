98d37d210887643126e59fd4842f7319033ef85f
Happy-Wang-01 <happyywang@uchicago.edu>
2024-01-08 19:53:50 -0600

    First part done in RS

---
+++ Install papaja.R
@@ -0,0 +1,24 @@
+install.packages("tidyverse")
+install.packages("papaja")
+require(papaja)
+install.packages("purrr")
+require(purrr)
+install.packages("psych")
+require(psych)
+#not sure what does "1+ other package of your choice" mean, therefore I installed and required another package
+#assign numeric variable
+x = 1
+x
+class(x)
+#assign string variable
+y = "hello"
+y
+class(y)
+#assign numeric variable in another way
+a <- 2
+a
+class(a)
+#assign character variable in another way
+b <- "yes"
+b
+class(b)
