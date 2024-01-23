e0f9b79f915554ee5f227eb1f9f9672acd4327a1
Happy-Wang-01 <happyywang@uchicago.edu>
2024-01-23 01:14:23 -0600

    Assignment 7 very brief description because this is not the final data
    @nrdowling @ysh627

--- data-prep.R
+++ data-prep.R
@@ -3,23 +3,35 @@ install.packages("haven")
 library(haven)
 Age_and_Possible_Selves<-read_sav("localonly/Age and Possible Selves 9.6.19.sav")
 #this dataset can be read in console
-#This dataset is not tidy. re
+#This dataset is not tidy.
+
+#create intermediate dataset using assign object "<-" and "%>%"
+#intermediate_AgeandPossibleSelves <- Age_and_Possible_Selves_9_6_19 %>%
+
 #We need to sort the data variables by scales not by scale items. 
-#All the possible selves measures, both quantitative and qualitative 
-#are meaningful while information on the participants' location, 
-#answer duration etc. are not necessary.
+#All the possible selves measures, 
+#both quantitative and qualitative are meaningful 
+#but leave out the quantitative data for data analysis; use filter()
+#while information on the participants' location, answer duration etc. 
+#are also not necessary. filter() out
 
 #use pivot_longer() to reshape the data
 #use drop_na() or replace_na() to clean up the data to make it functional for analysis
 
 #may need factor (), levles(), and fct_recode() for creating factors or recoding data
+#after recode, create the variables for analysis
+#summarize each scale;  use summarize()
+#for each scale, create an average value for each participants;use mean()
 
-#summarize each scale
-#for each scale, create an average value for each participants; use summarize(), mean()
-#leave out the quantitative data for data analysis; use filter()
-
-#will use regression, PROCESS models or extension models in jamovi for mediated moderation
-#test mediation, moderation, and mediated moderation
+#will use regression, PROCESS models or extension models in jamovi for mediated moderation analysis
+#test regression, mediation, moderation, and mediated moderation
 #curvilinear relationship therefor age_squared
+
+#graph out the potential models and results
+
 #the current dataset is not the final dataset that I will be using
 #therefore this is only a draft for the possible plan of attack on data analysis
+
+#after analysis
+#write into .sav file
+#write_sav(intermediate_AgeandPossibleSelves, "testing intermediate_AgeandPossibleSelves.sav")
