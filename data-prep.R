e4341f1b1a7d38427dc562bfd6643374fca4f04d
Happy-Wang-01 <happyywang@uchicago.edu>
2024-01-29 21:42:32 -0600

    Assginment 9 and 10 all in here
    @nrdowling @ysh627

--- data-prep.R
+++ data-prep.R
@@ -1,3 +1,8 @@
+#the current dataset is not the final dataset that I will be using, this is already a very preped dataset
+#but I do know how I will prepare my dataset because the type of data is very similar
+#I know what the raw data will be like and the following steps will be the steps I take to prep my data
+#therefore this is only a draft for the possible plan of attack on data analysis
+
 install.packages("haven")
 #importing datasets
 library(haven)
@@ -6,15 +11,13 @@ Age_and_Possible_Selves<-read_sav("localonly/Age and Possible Selves 9.6.19.sav"
 #This dataset is not tidy.
 
 #create intermediate dataset using assign object "<-" and "%>%"
-#intermediate_AgeandPossibleSelves <- Age_and_Possible_Selves_9_6_19 %>%
+#intermediate_AgeandPossibleSelves_1 <- Age_and_Possible_Selves_9_6_19 %>%
 
 #We need to sort the data variables by scales not by scale items. 
 #All the possible selves measures, 
 #both quantitative and qualitative are meaningful 
 #but leave out the quantitative data for data analysis; use filter()
-#while information on the participants' location, answer duration etc. 
-#are also not necessary. filter() out
-
+#while information on the participants' location, answer duration etc. are also not necessary. filter() out
 #use pivot_longer() to reshape the data
 #use drop_na() or replace_na() to clean up the data to make it functional for analysis
 
@@ -22,15 +25,17 @@ Age_and_Possible_Selves<-read_sav("localonly/Age and Possible Selves 9.6.19.sav"
 #after recode, create the variables for analysis
 #summarize each scale;  use summarize()
 #for each scale, create an average value for each participants;use mean()
+#mutate() , rename() will also be used a lot for a value for the scale
 
 #will use regression, PROCESS models or extension models in jamovi for mediated moderation analysis
 #test regression, mediation, moderation, and mediated moderation
 #curvilinear relationship therefor age_squared
 
 #graph out the potential models and results
-
-#the current dataset is not the final dataset that I will be using
-#therefore this is only a draft for the possible plan of attack on data analysis
+#will need a moderated mediation model, a curvilinear model, and some tables to display the result
+#ggplot2()+geom_point()+geom_smooth() for the simple mediation, simple moderation
+#the curvilinear regression model i can use ggplot2()+geom_line() or plot(lm())
+#for the moderated mediation model, I will need to use sem and semPlot
 
 #after analysis
 #write into .sav file
